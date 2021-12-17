430970dc1ab0bd3a839a0ab147a22a1464ca0b01
mathMakesArt <68834165+mathMakesArt@users.noreply.github.com>
2021-12-17 03:40:01 -0500

    Add files referenced in README
    Created the following files, as referenced in README.md:
    rc.local (autorun instructions on startup)
    safe_shutdown.py (detection of communication from Arduino Nano)
    nano_power_monitor.ino (Arduino Nano script to signal RPi GPIO)
    erigon.conf (supervisor service configuration for erigon)
    rpcdaemon.conf (supervisor service configuration for rpcdaemon)

---
+++ nano_power_monitor.ino
@@ -0,0 +1,9 @@
+void setup() {
+  // put your setup code here, to run once:
+  digitalWrite(2, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ safe_shutdown.py
@@ -0,0 +1,61 @@
+import pigpio
+import time
+import subprocess
+
+# CONFIGURATION VARIABLES
+GPIO_NUM = 24
+WAIT_SECONDS = 5.0
+SHUTDOWN_SECONDS = 300.0
+FLAG_INITIAL_SYNC = True
+
+def main():
+
+    # Initialization of variables with default values
+    flagPinOff = False
+    flagShutdown = False
+    offSecondsCount = 0.0
+    pi = pigpio.pi()
+
+    # Exit early if unable to establish a GPIO connection
+    if not pi.connected:
+        print("UNABLE TO CONNECT TO GPIO.")
+        exit()
+
+    # Set pin mode and pulldown resistor state
+    pi.set_mode(GPIO_NUM, pigpio.INPUT)
+    pi.set_pull_up_down(GPIO_NUM, pigpio.PUD_DOWN)
+
+    # Loop until power is lost for greater than SHUTDOWN_SECONDS seconds
+    while(flagShutdown == False):
+        currentVal = pi.read(GPIO_NUM)                 # Read the designated pin
+        flagPinOff = (currentVal == 0)           # Update flagPinOff
+        print("currentVal = " + str(currentVal) + ", offSecondsCount = " + str(offSecondsCount)) # Print to console
+        time.sleep(WAIT_SECONDS)                 # Wait for WAIT_SECONDS seconds
+
+        # If the designated pin detects no signal, increment offSecondsCount and check against SHUTDOWN_SECONDS
+        if flagPinOff:
+            offSecondsCount += WAIT_SECONDS
+            if (offSecondsCount >= SHUTDOWN_SECONDS):
+                flagShutdown = True
+        # Reset offSecondsCount if the designated pin detects a signal
+        else:
+            offSecondsCount = 0
+
+    # Set of actions to carry out during safe shutdown routine
+    # 1. Ending the supervisor task
+    print("\n\n\nENDING SUPERVISOR WITH: sudo systemctl stop supervisor\n\n\n")
+    stopSupervisor = subprocess.Popen(["sudo", "systemctl", "stop", "supervisor"], stdout=subprocess.PIPE)
+    print(stopSupervisor.communicate())
+
+    # 2. Clearing the Erigon data directory (ONLY in initial sync mode)
+    if FLAG_INITIAL_SYNC:
+        print("\n\n\nENDING SUPERVISOR WITH: sudo rm -rf /home/ethereum/.erigon\n\n\n")
+        clearData = subprocess.Popen(["sudo", "rm", "-rf", "/home/ethereum/.erigon"], stdout=subprocess.PIPE)
+        print(clearData.communicate())
+
+    # 3. Safe shutdown of NASPi device
+    print("\n\n\nINITIALIZING SAFE SHUTDOWN WITH: sudo /usr/local/bin/x-c1-softsd.sh\n\n\n")
+    safeShutdown = subprocess.Popen(["sudo", "/usr/local/bin/x-c1-softsd.sh"], stdout=subprocess.PIPE)
+    print(safeShutdown.communicate())
+
+main()
